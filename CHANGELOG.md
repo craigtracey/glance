fa2600daa6577041fc2fd36cc18ae4d9b7c30429
Matt Ray <matthewhray@gmail.com>
2013-01-09 11:50:47 -0600

    PR1 from John Dewey and Jay Pipes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 * name in the metadata.rb
 * foodcritic FC043 cleanups
+* Added debug and verbose as attributes (John Dewey)
+* Added searched role names as attributes (Jay Pipes)
 
 ## v2012.1.0
 
--- attributes/default.rb
+++ attributes/default.rb
@@ -22,6 +22,12 @@
 default["developer_mode"] = false  # we want secure passwords by default
 ########################################################################
 
+# This is the name of the Chef role that will install the Keystone Service API
+default["glance"]["keystone_service_chef_role"] = "keystone"
+
+# This is the name of the Chef role that will install the Glance API service
+default["glance"]["glance_api_chef_role"] = "glance-api"
+
 default["glance"]["verbose"] = "False"
 default["glance"]["debug"] = "False"
 
